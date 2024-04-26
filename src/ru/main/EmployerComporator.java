331f29f46abaf1f506e1f2320bb7104735f73f3c
VadimBoz <werbel34@mail.ru>
2024-04-26 19:36:45 +0300

    hw3

--- src/ru/main/EmployerComporator.java
+++ src/ru/main/EmployerComporator.java
@@ -8,7 +8,7 @@ public class EmployerComporator implements Comparator<Employer> {
     @Override
     public int compare(Employer o1, Employer o2) {
         if (o1 != null && o2!= null) {
-            if (Math.abs(o1.calcSalary() - o2.calcSalary()) < 0.001) {
+            if (Math.abs(o1.calcSalary() - o2.calcSalary()) <= 0.001) {
                 return o1.getName().compareTo(o2.getName());
             }
             else if ((o1.calcSalary() - o2.calcSalary()) > 0.001) return 1;
